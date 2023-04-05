0d730d2d8d457193f2ade8f7d567bfff3b33ecc9
syedaumaima <syedaumema83@gmail.com>
2023-04-05 17:11:53 +0500

    FEAT: JS Practise Day One

---
+++ Script.html/app.js
@@ -0,0 +1,197 @@
+alert(`JS practice day one`);
+
+/*document.write(`Hi there`);
+
+document.write(`6+6`);
+
+document.write(`<h1> this is heading 1 <h2>`);*/
+
+//Chapter 2
+
+var name = "maya";
+name = "dad"
+
+var nationality;
+nationality = "Pakitani";
+
+var floogle = "maya"
+
+var LessonAthor = "maya";
+
+var girlWhoKeepsSayingHerOwnName = "maya"
+
+var x = "maya";
+
+var lastname = "_Smith_";
+
+var CityOfOrgin ="_New Orleans_";
+
+var aussieGreeting = "_g ` Day_";
+
+var nickname = "Lilly";
+
+alert("Thanks for your input!");
+
+var thanx = "Thanks for your input";
+
+
+//Chapter 3 Variables Of Numbers
+
+var weight = 150;
+
+weight + 29
+
+/*1   var originalNum = 23;
+2   var newNum = originalNum + 7;
+
+var 1stPresident = "Washington";
+
+var prezWhoCame!st = "Washington";
+
+alert(
+    144
+)
+
+1 var caseQty = 144;
+2 alert(caseQty);*/
+
+
+//Chapter 4 Variables Name Legal and Illegal
+
+/*Exampe:
+
+userResponse
+userResponseTime
+userResponseTimeLimit
+respinse*/
+
+
+//Chapter 5 Math expressions : Familiar operators
+
+var popularNumber = 4;
+var popularNumber = 2 + 2;
+
+alert(2+2);
+
+varpopularNumber = 12 - 4;
+varpopularNumber = 3 * 12;
+
+/*1 var num  = 10;
+2 var varpopularNumber = num= 200;
+
+1 var num  = 10;
+2 var anotherNum = 1;
+2 var varpopularNumber = num + anotherNum;*/
+
+
+var whatsLeftOver = 10 % 3 ;
+
+var whatsLeftOver = 9 % 3 ;
+
+//Chapter 6 
+
+num++;
+
+num = num + 1;
+
+num--;
+
+
+/*1 var num = 1;
+2 var newNum = num++;
+
+1 var num = 1;
+2 var newNum = ++num;
+
+1 var num = 1;
+2 var newNum = num--;
+
+1 var num = 1;
+2 var newNum = --num;*/
+
+//Chapter 7
+
+var totalCost= 1 + (3*4);
+
+var totalCost= (1+3) * 4 ;
+
+resultOfComputation = ((2 * 4) * 4) + 2;
+
+resultOfComputation = (2*4) * (4*2);
+
+//Chapter 8 Concatenating text strings
+
+/*1 var message = "Thanks for your input!";
+2 alert(message);
+
+alert("Thanks, " + userName + "!");
+
+1 var message = "Thanks, ";
+2 var banger = "!";
+3 alert(message + userName + banger);*/
+
+alert("Thanks, " + "Susan" + "!");
+
+/*1 var message = "Thanks, ";
+2 var userName = "Susan";
+3 var banger = "!";
+4 var customMess = message + userName + banger;
+5 alert(customMess);*/
+
+alert("2" + "2");
+
+alert("2 plus 2 equals " + 2 + 2);
+
+//9 Prompts
+
+var spec = prompt("Your species?", "human");
+
+/*1 var question = "Your species?";
+2 var defaultAnswer = "human";
+3 var spec = prompt(question, defaultAnswer);
+
+1 var numberOfCats = prompt("How many cats?");
+2 var tooManyCats = numberOfCats + 1;*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
